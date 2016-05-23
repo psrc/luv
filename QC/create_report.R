67369fa6574077f8f44591de823354697d731ae6
Hana Sevcikova <hsevcikova@psrc.org>
2016-05-23 13:20:05 -0700

    QC framework prototype

---
+++ QC/create_report.R
@@ -0,0 +1,29 @@
+# Prototype for creating a QC report
+options(stringsAsFactors=FALSE)
+inputs <- read.table('inputs.txt', sep='\t', header=FALSE, col.names=c('par', 'value'), row.names=1)
+
+indicator.names <- c('households', 'population', 'employment')
+geographies <- c('faz', 'zone')
+indicator.path.run1 <- file.path(inputs['directory',], inputs['run1',], 'indicators')
+
+# Check decreases
+result <- NULL
+years <- c(2014, 2040)
+for (ind in indicator.names) {
+	for (geo in geographies) {
+		ind.values <- read.table(file.path(indicator.path.run1, paste0(geo, '__table__', ind, '.csv')), sep=',', header=TRUE)
+		dif <- ind.values[,paste(ind,years[2],sep='_')] - ind.values[,paste(ind,years[1],sep='_')]
+		negatives <- which(dif < 0)
+		if(length(negatives)>0) {
+			this.result <- cbind(ind.values[negatives, paste(geo, 'id', sep="_")], dif[negatives], round(dif[negatives]/ind.values[negatives,paste(ind,years[1],sep='_')]*100,2))
+			this.result <- data.frame(indicator=rep(ind, length(negatives)), geography=rep(geo, length(negatives)), this.result)
+			result <- rbind(result, this.result)
+		}
+	}
+}
+colnames(result)[3:ncol(result)] <- c('geo_id', 'difference', 'percent')
+
+subs.faz <- subset(result, geography == 'faz' & abs(percent) > 10 & abs(difference) > 50)
+subs.faz <- subs.faz[order(subs.faz$percent, decreasing=FALSE),]
+print(subs.faz)
+write.table(subs.faz, 'report.txt', sep='\t', row.names=FALSE)
