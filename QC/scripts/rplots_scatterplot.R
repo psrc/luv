964e0358684bb6e3deddf4ad34794fe5bfb3f9ff
Christy Lam <cyyllam@gmail.com>
2016-06-07 17:16:43 -0700

    incorporated loop for geographies and merging with zone and faz lookup tables

--- QC/scripts/rplots_scatterplot.R
+++ QC/scripts/rplots_scatterplot.R
@@ -4,8 +4,8 @@ library(plotly)
 library(htmlwidgets)
 
 #environment inputs
-attribute <- c("households","population", "employment", "residential_units")
-geography <- "zone"
+attribute <- c("population", "households","employment", "residential_units")
+geography <- c("zone", "faz")
 year1 <- (2040)
 year2 <- (2040)
 extension <- ".csv"
@@ -23,54 +23,64 @@ if(make) {
     run2 <- "run_170.run_2015_09_15_16_02" 
     run.name <- 'run71'
     result.dir <- file.path("C:/Users/Christy/Desktop/luv/QC/results", run.name)
+    faz.lookup <- read.table("C:/Users/Christy/Desktop/luv/QC/data/faz_names.txt", header =TRUE, sep = "\t")
+    zone.lookup <- read.table("C:/Users/Christy/Desktop/luv/QC/data/zones.txt", header =TRUE, sep = "\t")
 }
 
 runname1 <- unlist(strsplit(run1,"[.]"))[[1]]
 runname2 <- unlist(strsplit(run2,"[.]"))[[1]]
 if(!dir.exists(result.dir)) dir.create(result.dir)
 
-for (i in 1:length(attribute)){
-  #run1
-  filename1 <- paste0(geography,'__',"table",'__',attribute[i], extension)
-  datatable1 <- read.csv(file.path(base.dir, run1,"indicators",filename1), header = TRUE, sep = ",")
-  column_id <- colnames(datatable1)[grepl("_id",names(datatable1))]
-  column_est <-colnames(datatable1)[grepl(year1,names(datatable1))]
-  table1 <- datatable1[,c(column_id,column_est)]
-  colnames(table1)[2] <- paste0("estrun1")
-  
-  #run2
-  filename2 <- paste0(geography,'__',"table",'__',attribute[i], extension)
-  datatable2 <- read.csv(file.path(base.dir, run2,"indicators",filename2), header = TRUE, sep = ",")
-  column_id2 <- colnames(datatable2)[grepl("_id",names(datatable2))]
-  column_est2 <-colnames(datatable2)[grepl(year2,names(datatable2))]
-  table2 <- datatable2[,c(column_id2,column_est2)]
-  colnames(table2)[2] <- paste0("estrun2")
+for (a in 1:length(geography)){
+  for (i in 1:length(attribute)){
+    #run1
+    filename1 <- paste0(geography[a],'__',"table",'__',attribute[i], extension)
+    datatable1 <- read.csv(file.path(base.dir, run1,"indicators",filename1), header = TRUE, sep = ",")
+    column_id <- colnames(datatable1)[grepl("_id",names(datatable1))]
+    column_est <-colnames(datatable1)[grepl(year1,names(datatable1))]
+    table1 <- datatable1[,c(column_id,column_est)]
+    colnames(table1)[2] <- paste0("estrun1")
+    
+    #run2
+    filename2 <- paste0(geography[a],'__',"table",'__',attribute[i], extension)
+    datatable2 <- read.csv(file.path(base.dir, run2,"indicators",filename2), header = TRUE, sep = ",")
+    column_id2 <- colnames(datatable2)[grepl("_id",names(datatable2))]
+    column_est2 <-colnames(datatable2)[grepl(year2,names(datatable2))]
+    table2 <- datatable2[,c(column_id2,column_est2)]
+    colnames(table2)[2] <- paste0("estrun2")
 
+    #merge tables
+    merge.table <- merge(table1, table2, by = colnames(datatable2)[grepl("_id",names(datatable2))])
+    if (geography[a]=="zone"){
+      merge.table <- merge(merge.table, zone.lookup, "zone_id")
+      merge.table <- merge(merge.table, faz.lookup, "faz_id")
+      merge.table <- merge.table[,c(2, 1, 3:(ncol(merge.table)))] 
+    }else{
+        merge.table <- merge(merge.table, faz.lookup, "faz_id")
+      }  
   
-  #merge tables
-  merge.table <- merge(table1, table2, by = colnames(datatable2)[grepl("_id",names(datatable2))])
-
-  #plot
-  p <- plot_ly(merge.table,
-               x = estrun1,
-               y = estrun2,
-               text = paste0("Zone ", merge.table[,1]),
-               type = 'scatter',
-               mode = 'markers'
-               )%>%
-      layout(font = list(family="Segoe UI", size = 13.5),
-             title = paste0(as.name(attribute[i]), " by ", geography),
-             xaxis = list(title = runname1),
-             yaxis = list(title = runname2),
-             margin = list(l=100, b=100, t=90, r=100)
-              )
-  
-  #print (p)
-  print (paste0("Plotting ", as.name(attribute[i])))
-  htmlwidgets::saveWidget(as.widget(p),file.path(result.dir, paste0('rplots_', as.name(attribute[i]),"_scatterplot.html")))
+    #plot
+    p <- plot_ly(merge.table,
+                 x = estrun1,
+                 y = estrun2,
+                 text = paste0("ID: ", merge.table[,1], " FAZ Name: ", merge.table[,ncol(merge.table)]),
+                 type = 'scatter',
+                 mode = 'markers'
+                 )%>%
+        layout(font = list(family="Segoe UI", size = 13.5),
+               title = paste0(as.name(attribute[i]), " by ", as.name(geography[a])),
+               xaxis = list(title = runname1),
+               yaxis = list(title = runname2),
+               margin = list(l=100, b=100, t=90, r=100)
+                )
+    
+    #print (p)
+    print (paste0("Plotting ", as.name(attribute[i]), " by ", as.name(geography[a])))
+    htmlwidgets::saveWidget(as.widget(p),file.path(result.dir, paste0('rplots_', as.name(attribute[i]),"_",as.name(geography[a]),"_scatterplot.html")))
+  }
 }
-
 print ("Plotting complete! Check results directory.")
+
 
 
 
