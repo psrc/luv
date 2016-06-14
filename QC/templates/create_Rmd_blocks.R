8054911df3753299f98ac791dcf5b96990484817
Hana Sevcikova <hsevcikova@psrc.org>
2016-06-14 15:22:02 -0700

    script for AU per acre in RGCs

--- QC/templates/create_Rmd_blocks.R
+++ QC/templates/create_Rmd_blocks.R
@@ -7,7 +7,11 @@ create.header <- function(file, title, author=NULL, date=NULL, output="html_docu
 		cat("date:", date, "\n", file=file, append=TRUE)
 	cat("output:\n", file=file, append=TRUE)
 	cat(" ", output, ":\n", file=file, append=TRUE)
+	cat("    toc: true\n", file=file, append=TRUE)
 	cat("    theme:", theme, "\n", file=file, append=TRUE)
+	cat("    pandoc_args: [\n", file=file, append=TRUE)
+	cat("    	'--columns=500'\n", file=file, append=TRUE)
+	cat("    			]\n", file=file, append=TRUE)
 	cat("---\n", file=file, append=TRUE)
 }
 
@@ -37,4 +41,26 @@ add.table <- function(file, df){
 		cat("\n", file=file, append=TRUE)
 	}
 	#add.text(file, "```")
+	cat("\n", file=file, append=TRUE)
 }
+
+add.table.highlight <- function(file, df, highlight=c(), color='yellow'){
+	cat(paste(names(df), collapse = "|"), file=file, append=TRUE)
+	cat("\n", file=file, append=TRUE)
+	cat(paste(rep("-", ncol(df)), collapse = "|"), file=file, append=TRUE)
+	cat("\n", file=file, append=TRUE)
+	for(i in 1:nrow(df)){
+		if(i %in% highlight) {
+			for(j in 1:ncol(df)) {
+				cat(paste0('<span style="background:', color, '">'), file=file, append=TRUE)
+				cat(df[i,j], file=file, append=TRUE)
+				cat("</span>", file=file, append=TRUE)
+				if(j < ncol(df))cat("|", file=file, append=TRUE)
+			}
+		} else 
+			cat(paste(df[i,], collapse = "|"), file=file, append=TRUE)
+		cat("\n", file=file, append=TRUE)
+	}
+	cat("\n", file=file, append=TRUE)
+}
+
