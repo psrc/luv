d674f79f5af3a6c5e1f393b3fc4ec7c30d0b8ced
Christy Lam <cyyllam@gmail.com>
2016-06-15 15:24:06 -0700

    expanded geography loop functionality

--- QC/scripts/rplots_scattergeo.R
+++ QC/scripts/rplots_scattergeo.R
@@ -1,10 +1,11 @@
-#This script will produce scattermaps in html comparing 2040 estimates from runs that are specified in inputs.txt
+# This script will produce scattermaps in html comparing 2040 estimates from runs that are specified in inputs.txt
 
 library(plotly)
 library(htmlwidgets)
+library(RColorBrewer)
 
 attribute <- c("population", "households","employment", "residential_units")
-geography <- c("zone")#, "faz")
+geography <- c("zone","faz")
 year1 <- (2040)
 year2 <- (2040)
 extension <- ".csv"
@@ -42,11 +43,14 @@ index.file <- file.path(result.dir, 'rplots_scattermap.Rmd')
 if(file.exists(index.file)) unlink(index.file)
 create.section(index.file, title=paste("Scattermaps for ", runname1, "and", runname2))
 
-diff.table <- NULL
+
 
 for (a in 1:length(geography)){
+  
+  diff.table <- NULL
+  
   for (i in 1:length(attribute)){
-    #run1
+    # run1
     filename1 <- paste0(geography[a],'__',"table",'__',attribute[i], extension)
     datatable1 <- read.csv(file.path(base.dir, run1,"indicators",filename1), header = TRUE, sep = ",")
     column_id <- colnames(datatable1)[grepl("_id",names(datatable1))]
@@ -54,7 +58,7 @@ for (a in 1:length(geography)){
     table1 <- datatable1[,c(column_id,column_est)]
     colnames(table1)[2] <- paste0("estrun1")
     
-    #run2
+    # run2
     filename2 <- paste0(geography[a],'__',"table",'__',attribute[i], extension)
     datatable2 <- read.csv(file.path(base.dir, run2,"indicators",filename2), header = TRUE, sep = ",")
     column_id2 <- colnames(datatable2)[grepl("_id",names(datatable2))]
@@ -62,18 +66,18 @@ for (a in 1:length(geography)){
     table2 <- datatable2[,c(column_id2,column_est2)]
     colnames(table2)[2] <- paste0("estrun2")
     
-    #merge tables
+    # merge tables
     merge.table <- merge(table1, table2, by = colnames(datatable2)[grepl("_id",names(datatable2))])
     merge.table <- cbind(merge.table, diff=merge.table$estrun1-merge.table$estrun2, indicator=as.character(attribute[i]))
     
-    #select largest differences
+    # select largest differences
     merge.table <- merge.table[order(merge.table$diff),]
     top <- head(merge.table, n=15)
     bottom <- tail(merge.table, n=15)
     ifelse (is.null(diff.table),diff.table <- rbind(top, bottom), diff.table <- rbind(diff.table, top, bottom))
-  } #end of attribute loop
+  } # end of attribute loop
   
-  #merge with name lookups and xy
+  # merge with lookup tables
   if (geography[a]=="zone"){
     drops <- c("area_type_id", "district_id")
     combine.lookup <- merge(zone.lookup, faz.lookup, by = "faz_id")
@@ -81,6 +85,9 @@ for (a in 1:length(geography)){
     new.zonexy.lookup <- zonexy.lookup[,!(names(zonexy.lookup) %in% drops)]
     combine.lookup <- merge(combine.lookup, new.zonexy.lookup, by = c("zone_id","faz_id"))
     diff.table <- merge(diff.table, combine.lookup, by="zone_id")
+  } else {
+    combine.lookup <- merge(faz.lookup, fazxy.lookup, by = "faz_id")
+    diff.table <- merge(diff.table, combine.lookup, by = "faz_id")
   }
 
  # common map properties
@@ -97,7 +104,10 @@ for (a in 1:length(geography)){
  # id for anchoring traces on different plots
  diff.table$id <- as.integer(factor(diff.table$indicator))
  
- #plot 
+ # hover info
+ diff.table$hover <- with(diff.table, paste("ID: ", diff.table[,1], Name, '<br>',"2040 est. difference: ", diff))
+ 
+ # plot 
  p <- plot_ly(diff.table, 
              type = 'scattergeo', 
              lon = long, 
@@ -105,19 +115,18 @@ for (a in 1:length(geography)){
              geo = paste0("geo", id),
              group = indicator,
              name = as.character(attribute[i]),
+             text = hover,
              showlegend = T,
-             marker = list(size=(abs(diff))/100, opacity = 0.5)) %>%
+             marker = list(size=(abs(diff))/120, opacity = 0.5)) %>%
       layout(font = list(family="Segoe UI", size = 13.5),
             title = paste0('Greatest differences in 2040 estimates between ', runname1, " and ", runname2, " by ", geography[a]),
             geo = g,
-            autosize = F,
-            width = 1900,
-            height = 1000,
             margin = list(l=50, b=50, t=90, r=100),
-            hovermode = F)
+            hovermode = T)
   
  q <- subplot(p, nrows = 2)
  
+ # create html files
  subtitle <- paste0("Scattermap of all indicators by ", as.name(geography[a]))
  print (paste0("Plotting ", subtitle))
  html.file <- file.path(result.dir, paste0('rplots', "_", as.name(geography[a]), "_scattermap.html"))
@@ -126,7 +135,7 @@ for (a in 1:length(geography)){
  # add text into the index file
  add.text(index.file, paste0("* [", subtitle, "](", paste0('file://', html.file), ")"))
 
-} #end of geography loop
+} # end of geography loop
 
 
 
