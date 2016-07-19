d46ab44e6905409d1dd5bd3829bcbedd32092b7d
Hana Sevcikova <hsevcikova@psrc.org>
2016-07-18 18:02:15 -0700

    script for tractcity shares; map scripts renamed to rmaps_*

--- QC/scripts/create_indicator_files.py
+++ QC/scripts/create_indicator_files.py
@@ -25,82 +25,82 @@ def get_indicators(cache_directory, run_description, years = [2014,2015,2020,202
     # FAZ indicators 
     # =====================
     
-       Table(
-           attribute = 'households=faz.aggregate(urbansim_parcel.building.number_of_households, intermediates=[parcel,zone])',
-           dataset_name = 'faz',
-           source_data = source_data,
-           ),
-       Table(
-           attribute = 'population=faz.aggregate(urbansim_parcel.building.population, intermediates=[parcel,zone])',
-           dataset_name = 'faz',
-           source_data = source_data,
-           ),
-       Table(
-           attribute = 'employment=faz.aggregate(urbansim_parcel.building.number_of_jobs, intermediates=[parcel,zone])',
-           dataset_name = 'faz',
-           source_data = source_data,
-           ),
-       # Table(
-           # attribute = 'nonres_sqft=faz.aggregate(urbansim_parcel.building.non_residential_sqft, intermediates=[parcel,zone])',
-           # dataset_name = 'faz',
-           # source_data = source_data,
-           # ),	   
-       Table(
-           attribute = 'residential_units=faz.aggregate(urbansim_parcel.building.residential_units, intermediates=[parcel,zone])',
-           dataset_name = 'faz',
-           source_data = source_data,
-           ),
-               
-    # TAZ indicators 
-       Table(
-           attribute = 'residential_units=zone.aggregate(urbansim_parcel.building.residential_units, intermediates=[parcel])',
-           dataset_name = 'zone',
-           source_data = source_data,
-           ),
-       Table(
-           attribute = 'households=zone.aggregate(urbansim_parcel.building.number_of_households, intermediates=[parcel])',
-           dataset_name = 'zone',
-           source_data = source_data,
-           ),
-       Table(
-           attribute = 'population=zone.aggregate(urbansim_parcel.building.population, intermediates=[parcel])',
-           dataset_name = 'zone',
-           source_data = source_data,
-           ),
-       Table(
-           attribute = 'employment=zone.aggregate(urbansim_parcel.building.number_of_jobs, intermediates=[parcel])',
-           dataset_name = 'zone',
-           source_data = source_data,
-           ),
-    
-    # ## City indicators
-    # ==================
-    
-       Table(
-           attribute = 'households=city.aggregate(urbansim_parcel.building.number_of_households, intermediates=[parcel])',
-           dataset_name = 'city',
-           source_data = source_data,
-           ),
-       Table(
-           attribute = 'population=city.aggregate(urbansim_parcel.building.population, intermediates=[parcel])',
-           dataset_name = 'city',
-           source_data = source_data,
-            ),
-       Table(
-           attribute = 'employment=city.aggregate(urbansim_parcel.building.number_of_jobs, intermediates=[parcel])',
-           dataset_name = 'city',
-           source_data = source_data,
-           ),
-       Table(
-           attribute = 'residential_units=city.aggregate(urbansim_parcel.building.residential_units, intermediates=[parcel])',
-           dataset_name = 'city',
-           source_data = source_data,
-           ),
        #Table(
-       #    attribute = 'acres=city.aggregate(parcel.parcel_sqft/43560.)',
-       #    dataset_name = 'city',
-       #    source_data = source_data,
-       #    ),        
+           #attribute = 'households=faz.aggregate(urbansim_parcel.building.number_of_households, intermediates=[parcel,zone])',
+           #dataset_name = 'faz',
+           #source_data = source_data,
+           #),
+       #Table(
+           #attribute = 'population=faz.aggregate(urbansim_parcel.building.population, intermediates=[parcel,zone])',
+           #dataset_name = 'faz',
+           #source_data = source_data,
+           #),
+       #Table(
+           #attribute = 'employment=faz.aggregate(urbansim_parcel.building.number_of_jobs, intermediates=[parcel,zone])',
+           #dataset_name = 'faz',
+           #source_data = source_data,
+           #),
+       ## Table(
+           ## attribute = 'nonres_sqft=faz.aggregate(urbansim_parcel.building.non_residential_sqft, intermediates=[parcel,zone])',
+           ## dataset_name = 'faz',
+           ## source_data = source_data,
+           ## ),	   
+       #Table(
+           #attribute = 'residential_units=faz.aggregate(urbansim_parcel.building.residential_units, intermediates=[parcel,zone])',
+           #dataset_name = 'faz',
+           #source_data = source_data,
+           #),
+               
+    ## TAZ indicators 
+       #Table(
+           #attribute = 'residential_units=zone.aggregate(urbansim_parcel.building.residential_units, intermediates=[parcel])',
+           #dataset_name = 'zone',
+           #source_data = source_data,
+           #),
+       #Table(
+           #attribute = 'households=zone.aggregate(urbansim_parcel.building.number_of_households, intermediates=[parcel])',
+           #dataset_name = 'zone',
+           #source_data = source_data,
+           #),
+       #Table(
+           #attribute = 'population=zone.aggregate(urbansim_parcel.building.population, intermediates=[parcel])',
+           #dataset_name = 'zone',
+           #source_data = source_data,
+           #),
+       #Table(
+           #attribute = 'employment=zone.aggregate(urbansim_parcel.building.number_of_jobs, intermediates=[parcel])',
+           #dataset_name = 'zone',
+           #source_data = source_data,
+           #),
+    
+    ## ## City indicators
+    ## ==================
+    
+       #Table(
+           #attribute = 'households=city.aggregate(urbansim_parcel.building.number_of_households, intermediates=[parcel])',
+           #dataset_name = 'city',
+           #source_data = source_data,
+           #),
+       #Table(
+           #attribute = 'population=city.aggregate(urbansim_parcel.building.population, intermediates=[parcel])',
+           #dataset_name = 'city',
+           #source_data = source_data,
+            #),
+       #Table(
+           #attribute = 'employment=city.aggregate(urbansim_parcel.building.number_of_jobs, intermediates=[parcel])',
+           #dataset_name = 'city',
+           #source_data = source_data,
+           #),
+       #Table(
+           #attribute = 'residential_units=city.aggregate(urbansim_parcel.building.residential_units, intermediates=[parcel])',
+           #dataset_name = 'city',
+           #source_data = source_data,
+           #),
+       ##Table(
+       ##    attribute = 'acres=city.aggregate(parcel.parcel_sqft/43560.)',
+       ##    dataset_name = 'city',
+       ##    source_data = source_data,
+       ##    ),        
 
     # ## Tract-City indicators
     # ==================
@@ -135,96 +135,96 @@ def get_indicators(cache_directory, run_description, years = [2014,2015,2020,202
     # ## Growth Centers Indicators
     # ============================
       
-       Table(
-           attribute = 'residential_units=growth_center.aggregate(urbansim_parcel.building.residential_units, intermediates=[parcel])',
-           dataset_name = 'growth_center',
-           source_data = source_data,
-           ),
-       Table(
-           attribute = 'households=growth_center.aggregate(urbansim_parcel.building.number_of_households, intermediates=[parcel])',
-           dataset_name = 'growth_center',
-           source_data = source_data,
-           ),
-      Table(
-          attribute = 'population=growth_center.aggregate(urbansim_parcel.building.population, intermediates=[parcel])',
-          dataset_name = 'growth_center',
-          source_data = source_data,
-           ),
-       Table(
-           attribute = 'employment=growth_center.aggregate(urbansim_parcel.building.number_of_jobs, intermediates=[parcel])',
-           dataset_name = 'growth_center',
-           source_data = source_data,
-           ),
        #Table(
-       #    attribute = 'acres=growth_center.aggregate(parcel.parcel_sqft/43560.)',
-       #    dataset_name = 'growth_center',
-       #    source_data = source_data,
-       #    ),       
+           #attribute = 'residential_units=growth_center.aggregate(urbansim_parcel.building.residential_units, intermediates=[parcel])',
+           #dataset_name = 'growth_center',
+           #source_data = source_data,
+           #),
+       #Table(
+           #attribute = 'households=growth_center.aggregate(urbansim_parcel.building.number_of_households, intermediates=[parcel])',
+           #dataset_name = 'growth_center',
+           #source_data = source_data,
+           #),
+      #Table(
+          #attribute = 'population=growth_center.aggregate(urbansim_parcel.building.population, intermediates=[parcel])',
+          #dataset_name = 'growth_center',
+          #source_data = source_data,
+           #),
+       #Table(
+           #attribute = 'employment=growth_center.aggregate(urbansim_parcel.building.number_of_jobs, intermediates=[parcel])',
+           #dataset_name = 'growth_center',
+           #source_data = source_data,
+           #),
+       ##Table(
+       ##    attribute = 'acres=growth_center.aggregate(parcel.parcel_sqft/43560.)',
+       ##    dataset_name = 'growth_center',
+       ##    source_data = source_data,
+       ##    ),       
     
-    # ## Large Area Indicators
-    # ============================
+    ## ## Large Area Indicators
+    ## ============================
     
-        DatasetTable(
-           source_data = source_data,
-           dataset_name = 'large_area',
-           name = 'employment_by_aggr_sector',
-           attributes = ['large_area.county_id'] + [
-               "construction_resources = psrc_parcel.large_area.number_of_jobs_of_sector_1 + psrc_parcel.large_area.number_of_jobs_of_sector_2",
-               "manuf_WTU = psrc_parcel.large_area.number_of_jobs_of_sector_3 + psrc_parcel.large_area.number_of_jobs_of_sector_4 + psrc_parcel.large_area.number_of_jobs_of_sector_5 + psrc_parcel.large_area.number_of_jobs_of_sector_6 + psrc_parcel.large_area.number_of_jobs_of_sector_8 + psrc_parcel.large_area.number_of_jobs_of_sector_9",
-               "retail_food_services = psrc_parcel.large_area.number_of_jobs_of_sector_7 + psrc_parcel.large_area.number_of_jobs_of_sector_14",
-               "FIRE_services = psrc_parcel.large_area.number_of_jobs_of_sector_12 + psrc_parcel.large_area.number_of_jobs_of_sector_10 + psrc_parcel.large_area.number_of_jobs_of_sector_11 + psrc_parcel.large_area.number_of_jobs_of_sector_13 + psrc_parcel.large_area.number_of_jobs_of_sector_15 + psrc_parcel.large_area.number_of_jobs_of_sector_16 + psrc_parcel.large_area.number_of_jobs_of_sector_17",
-               "government = psrc_parcel.large_area.number_of_jobs_of_sector_18",
-               "edu = psrc_parcel.large_area.number_of_jobs_of_sector_19"
-           ],
-           output_type = 'tab'
-               ),    
+        #DatasetTable(
+           #source_data = source_data,
+           #dataset_name = 'large_area',
+           #name = 'employment_by_aggr_sector',
+           #attributes = ['large_area.county_id'] + [
+               #"construction_resources = psrc_parcel.large_area.number_of_jobs_of_sector_1 + psrc_parcel.large_area.number_of_jobs_of_sector_2",
+               #"manuf_WTU = psrc_parcel.large_area.number_of_jobs_of_sector_3 + psrc_parcel.large_area.number_of_jobs_of_sector_4 + psrc_parcel.large_area.number_of_jobs_of_sector_5 + psrc_parcel.large_area.number_of_jobs_of_sector_6 + psrc_parcel.large_area.number_of_jobs_of_sector_8 + psrc_parcel.large_area.number_of_jobs_of_sector_9",
+               #"retail_food_services = psrc_parcel.large_area.number_of_jobs_of_sector_7 + psrc_parcel.large_area.number_of_jobs_of_sector_14",
+               #"FIRE_services = psrc_parcel.large_area.number_of_jobs_of_sector_12 + psrc_parcel.large_area.number_of_jobs_of_sector_10 + psrc_parcel.large_area.number_of_jobs_of_sector_11 + psrc_parcel.large_area.number_of_jobs_of_sector_13 + psrc_parcel.large_area.number_of_jobs_of_sector_15 + psrc_parcel.large_area.number_of_jobs_of_sector_16 + psrc_parcel.large_area.number_of_jobs_of_sector_17",
+               #"government = psrc_parcel.large_area.number_of_jobs_of_sector_18",
+               #"edu = psrc_parcel.large_area.number_of_jobs_of_sector_19"
+           #],
+           #output_type = 'tab'
+               #),    
 
-## #### ============  Miscellaneous tables
-## #### ============  Checked pretty regularly 
-## #### ============  Run for every year in the simulation
+### #### ============  Miscellaneous tables
+### #### ============  Checked pretty regularly 
+### #### ============  Run for every year in the simulation
 
 
-## ##------Liming's Unplaced Households and Jobs in the Region-----------
+### ##------Liming's Unplaced Households and Jobs in the Region-----------
 
-    Table(
-           attribute = 'num_unplaced_hhs=alldata.aggregate_all(household.building_id<=0)',
-           dataset_name = 'alldata',
-           source_data = source_data,
-         ),
-    Table(
-           attribute = 'num_unplaced_jobs=alldata.aggregate_all(job.building_id<=0)',
-           dataset_name = 'alldata',
-           source_data = source_data,
-         ),
+    #Table(
+           #attribute = 'num_unplaced_hhs=alldata.aggregate_all(household.building_id<=0)',
+           #dataset_name = 'alldata',
+           #source_data = source_data,
+         #),
+    #Table(
+           #attribute = 'num_unplaced_jobs=alldata.aggregate_all(job.building_id<=0)',
+           #dataset_name = 'alldata',
+           #source_data = source_data,
+         #),
     
-    #  Regional Total Tables	 
+    ##  Regional Total Tables	 
              
              
-      Table(
-          attribute = 'residential_units=alldata.aggregate_all(urbansim_parcel.building.residential_units)',
-          dataset_name = 'alldata',
-          source_data = source_data,
-          ),
-      Table(
-          attribute = 'non_residential_sqft=alldata.aggregate_all(urbansim_parcel.building.non_residential_sqft)',
-          dataset_name = 'alldata',
-          source_data = source_data,
-          ),
-      Table(
-          attribute = 'households=alldata.aggregate_all(urbansim_parcel.building.number_of_households)',
-          dataset_name = 'alldata',
-          source_data = source_data,
-          ),
-      Table(
-          attribute = 'employment=alldata.aggregate_all(urbansim_parcel.building.number_of_jobs)',
-          dataset_name = 'alldata',
-          source_data = source_data,
-          ),
-      Table(
-          attribute = 'population=alldata.aggregate_all(urbansim_parcel.building.population)',
-          dataset_name = 'alldata',
-          source_data = source_data,
-          ),
+      #Table(
+          #attribute = 'residential_units=alldata.aggregate_all(urbansim_parcel.building.residential_units)',
+          #dataset_name = 'alldata',
+          #source_data = source_data,
+          #),
+      #Table(
+          #attribute = 'non_residential_sqft=alldata.aggregate_all(urbansim_parcel.building.non_residential_sqft)',
+          #dataset_name = 'alldata',
+          #source_data = source_data,
+          #),
+      #Table(
+          #attribute = 'households=alldata.aggregate_all(urbansim_parcel.building.number_of_households)',
+          #dataset_name = 'alldata',
+          #source_data = source_data,
+          #),
+      #Table(
+          #attribute = 'employment=alldata.aggregate_all(urbansim_parcel.building.number_of_jobs)',
+          #dataset_name = 'alldata',
+          #source_data = source_data,
+          #),
+      #Table(
+          #attribute = 'population=alldata.aggregate_all(urbansim_parcel.building.population)',
+          #dataset_name = 'alldata',
+          #source_data = source_data,
+          #),
     
     ]
     return indicators
---
+++ QC/scripts/rplots_scatterplot_tractcity.R
@@ -0,0 +1,155 @@
+#This script will produce scatterplots in html comparing 2040 estimates from runs that are specified in inputs.txt
+
+library(plotly)
+library(htmlwidgets)
+library(data.table)
+
+#environment inputs
+attribute <- c("population", "households","employment", "residential_units")
+geography <- c("tractcity")
+year1 <- (2040)
+year2 <- (2040)
+extension <- ".csv"
+
+trim <- function (x) gsub("^\\s+|\\s+$", "", x) # function for triming whitespace 
+
+make <- !interactive()
+if(make) {
+  base.dir <- Sys.getenv('QC_BASE_DIRECTORY')
+  run1 <- Sys.getenv('QC_RUN1')
+  run2.all <- Sys.getenv('QC_RUN2')
+  run2.all <- trim(unlist(strsplit(run2.all, ","))) # run2 can have multiple directories; split by comma
+  result.dir <- Sys.getenv('QC_RESULT_PATH')
+} else {
+  base.dir <- "/Volumes/e$/opusgit/urbansim_data/data/psrc_parcel/runs"
+  run1 <- "run_82.run_2016_07_11_16_07"
+  #run1 <- "run_81.run_2016_07_05_16_00"
+  run2.all <- c("run_81.run_2016_07_05_16_00", "luv_1.compiled")
+  run.name <- 'run82'
+  result.dir <- "results"
+}
+
+lookup <- read.table(file.path("data", "tractcity.csv"), header =TRUE, sep = ",")
+city.lookup <- read.table(file.path("data", 'cities.csv'), header=TRUE, sep=',')
+  
+runname1 <- unlist(strsplit(run1,"[.]"))[[1]]
+runnames2 <- sapply(strsplit(run2.all,"[.]"), function(x) x[1]) # can have multiple values
+if(!dir.exists(result.dir)) dir.create(result.dir)
+
+# put a header into the index file
+source('templates/create_Rmd_blocks.R')
+index.file <- file.path(result.dir, 'rplots_scatter_tractcity.Rmd')
+if(file.exists(index.file)) unlink(index.file)
+create.section(index.file, title="Scatterplots for city shares by tractcity")
+
+luv1.comments <- read.table(file.path("data", "luv1_comments_tc14_part.csv"), header=TRUE, sep=",")
+luv1.elements <- sapply(strsplit(trim(as.character(luv1.comments$element)), "[.]"), function(x) x[1])
+
+for (irun in 1:length(run2.all)) {
+	run2 <- run2.all[irun]
+	runname2 <- runnames2[irun]
+	#add.text(index.file, paste("####", runname1, "vs.", runname2, "\n"))
+
+# build plotly table for subplot
+for (a in 1:length(geography)){
+  
+  indicators.table <- NULL
+  
+  for (i in 1:length(attribute)){
+    #run1
+    filename1 <- paste0(geography[a],'__',"table",'__',attribute[i], extension)
+    datatable1 <- read.csv(file.path(base.dir, run1,"indicators",filename1), header = TRUE, sep = ",")
+    column_id <- colnames(datatable1)[grepl("_id",names(datatable1))]
+    column_est <-colnames(datatable1)[grepl(year1,names(datatable1))]
+    table1 <- datatable1[,c(column_id,column_est)]
+    colnames(table1)[2] <- paste0("estrun1_raw")
+    table1 <- merge(table1, lookup[,c('city_id', 'tractcity_id','census_2010_tract_id')], by='tractcity_id')
+    table1.city <- data.table(table1)[,list(estrun1.city=sum(estrun1_raw)), by='city_id']
+    table1 <- merge(table1, table1.city, by='city_id')
+    table1$estrun1 <- round(100*table1$estrun1_raw/table1$estrun1.city,1)
+    table1 <- table1[,-which(colnames(table1) %in% c('estrun1.city'))]
+    
+    #run2
+    filename2 <- paste0(geography[a],'__',"table",'__',attribute[i], extension)
+    datatable2 <- read.csv(file.path(base.dir, run2,"indicators",filename2), header = TRUE, sep = ",")
+    column_id2 <- colnames(datatable2)[grepl("_id",names(datatable2))]
+    column_est2 <-colnames(datatable2)[grepl(year2,names(datatable2))]
+    table2 <- datatable2[,c(column_id2,column_est2)]
+    colnames(table2)[2] <- paste0("estrun2_raw")
+    table2 <- merge(table2, lookup[,c('city_id', 'tractcity_id')], by='tractcity_id')
+    table2.city <- data.table(table2)[,list(estrun2.city=sum(estrun2_raw)), by='city_id']
+    table2 <- merge(table2, table2.city, by='city_id')
+    table2$estrun2 <- round(100*table2$estrun2_raw/table2$estrun2.city,1)
+    table2 <- table2[,-which(colnames(table2) %in% c('city_id', 'estrun2.city'))]
+    
+    #merge tables
+    merge.table <- merge(table1, table2, by = 'tractcity_id')
+    merge.table <- merge(merge.table, city.lookup, "city_id")
+
+    merge.table$indicator <- switch(attribute[i],"population"="Population", "households"="Households", "employment"="Employment", "residential_units"="Residential Units")
+    luv1c <- subset(luv1.comments, luv1.elements == switch(attribute[i], "households"="household", "employment"="job", "XXX"))
+    #if(nrow(luv1c) > 0)
+    	merge.table <- merge(merge.table, luv1c, by.x="tractcity_id", by.y="tractcity_2014", all.x=TRUE)
+    
+    indicators.table <- if(is.null(indicators.table)) merge.table else rbind(indicators.table,merge.table)
+  
+  } # end of attribute loop
+  
+  # id for anchoring traces on different plots
+  indicators.table$id <- as.integer(factor(indicators.table$indicator))
+  indicators.table$name <- paste(substr(indicators.table$census_2010_tract_id, 6, 11), indicators.table$city_name)
+  
+  
+  
+  #plot
+  p <- plot_ly(indicators.table,
+               x = estrun1,
+               y = estrun2,
+               text = paste0("ID: ", indicators.table[,'tractcity_id'], " Name: ", indicators.table[,'name'],"<br>",
+               "Totals: (", indicators.table[,'estrun1_raw'], ", ", indicators.table[,'estrun2_raw'],"), Target: ", indicators.table[,'target_value']),
+               group = indicator,
+               xaxis = paste0("x", id),
+               type = 'scatter',
+               mode = 'markers'
+               )%>%
+      add_trace(x=c(0,max(estrun1)), 
+                y=c(0,max(estrun1)),
+                group = indicator,
+                xaxis = paste0("x", id),
+                marker = list(color="grey", size = 0),
+                opacity = .6,
+                mode = "lines",
+                showlegend = F)
+  
+  p <- layout(subplot(p, nrows=2),
+              xaxis = list(title = runname1, domain = c(0, .45), showgrid=TRUE),
+              yaxis = list(title = runname2, domain = c(.55, 1), tickfont=list(family="Segoe UI", size = 13)),
+              
+              xaxis2 = list(title = runname1, domain = c(.55, 1), showgrid=TRUE),
+              yaxis2 = list(title = runname2, domain = c(.55, 1), tickfont=list(family="Segoe UI", size = 13)),
+              
+              xaxis3 = list(title = runname1, domain = c(0, .45), showgrid=TRUE),
+              yaxis3 = list(title = runname2, domain = c(0, .45), tickfont=list(family="Segoe UI", size = 13)),
+              
+              xaxis4 = list(title = runname1, domain = c(.55, 1), showgrid=TRUE),
+              yaxis4 = list(title = runname2, domain = c(0, .45), tickfont=list(family="Segoe UI", size = 13)),
+              
+              font = list(family="Segoe UI", size = 13.5),
+              title = paste0(runname1," and ", runname2, " 2040 by ", as.name(geography[a]), ": percent shares within cities"),
+              margin = list(l=100, b=100, t=90, r=100)
+              )
+  
+  print (p)
+  subtitle <- paste(runname1, "vs.", runname2)
+  print (paste0("Plotting ", subtitle))
+  html.file <- file.path(result.dir, paste0("rplots_", runname2, "_", as.name(geography[a]), "_scatterplot.html"))
+  htmlwidgets::saveWidget(as.widget(p), html.file)
+  # add text into the index file
+  add.text(index.file, paste0("* [", subtitle, "](", paste0('file://', html.file), ")"))
+  
+} # end of geography loop
+#add.text(index.file, "\n\n")
+} # end of run2 loop
+
+print ("Plotting complete! Check results directory.")
+
