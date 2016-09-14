4a0ff1fee5755606f227df6a8dd87c91b85a21ac
Christy Lam <cyyllam@gmail.com>
2016-09-14 16:12:37 -0700

    flexdashboard Rmd file rename

--- QC/scripts/run_dash.R
+++ QC/scripts/run_dash.R
@@ -1,3 +1,3 @@
 library(rmarkdown)
 library(flexdashboard)
-run("scripts/rflex_scatterplot.Rmd", shiny_args=list(launch.browser=TRUE))
+run("scripts/rflex_dashboard.Rmd", shiny_args=list(launch.browser=TRUE))
