328835675f56549d215e44145a0da73eccf4f37e
Billy Charlton <billy@worldofbilly.com>
2016-10-10 10:48:24 -0700

    add the new "LUV Runner" script

---
+++ run-luv.sh
@@ -0,0 +1,40 @@
+# LUV RUNNER
+# ----------------------------------------------------
+URBANSIM_URL=https://github.com/psrc/urbansim.git
+URBANSIM_VERSION=master
+
+DB_GOLDEN_URL=
+DB_STAGING_URL=
+
+# ----------------------------------------------------
+# Install all dependencies: git, python, etc
+pip install -r requirements.txt
+
+# ----------------------------------------------------
+# Fetch urbansim
+git clone $URBANSIM_URL urbansim
+cd urbansim; git checkout $URBANSIM_VERSION ; cd ..
+
+# ----------------------------------------------------
+# Fetch golden standard database
+
+# ----------------------------------------------------
+# Stage all database changes
+
+# ----------------------------------------------------
+# Store snapshot somewhere
+
+# ----------------------------------------------------
+# Write out cache
+
+# ----------------------------------------------------
+# Check for sanity
+
+# ----------------------------------------------------
+# Run urbansim!
+
+# ----------------------------------------------------
+# Summarize everything
+
+# ----------------------------------------------------
+# Store summaries somewhere
