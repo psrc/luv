90342295b15222a789f81c8cb227599e62f03dd9
Christy Lam <cyyllam@gmail.com>
2017-03-08 15:42:10 -0800

    testing user inputs; htmlOutput not rendering

--- QC/shiny_dashboard/ui.R
+++ QC/shiny_dashboard/ui.R
@@ -1,7 +1,39 @@
 navbarPage(theme = shinytheme("readable"),
            "LUV QC Dashboard",
+           tabPanel("Selection",
+                    fluidPage(
+                      column(width = 4
+                        
+                      ), # end column
+                      column(width = 4,
+                             h4(class="header", checked=NA,
+                                tags$b("Select the following to view Dashboard content")
+                             ),
+                             br(),
+                             selectInput(inputId = "init_select_server",
+                                         label = "Server",
+                                         choices = c("modelsrv3" =1,
+                                                     "modelsrv8" =2)
+                             ),
+                             uiOutput("init_select_run1"), # dynamic, lists runs based on selected modelsrv
+                             uiOutput("init_select_run2all"), # dynamic, lists runs based on selected modelsrv
+                             uiOutput("init_select_resultsdir"), # dynamic, lists dirs in QC/results
+                             br(),
+                             actionButton("goButton", "Submit")
+                      ), # end column
+                      column(width = 4,
+                             verbatimTextOutput("out1")
+                        
+                      ) # end column
+                      
+                    ) # end fluidPage
+                    
+           ), # end tabPanel
            tabPanel("Index",
-                        includeHTML(file.path(result.dir,'index.html'))
+                    htmlOutput("index") # dynamic, lists index.html based on results directory input from selection page
+                    #uiOutput("index")#,
+                    # includeHTML('www/index.html')
+                    # includeHTML(file.path(result.dir,'index.html'))
            ), # end tabPanel
            tabPanel("Run Comparison",
                     fluidPage(
@@ -11,10 +43,7 @@ navbarPage(theme = shinytheme("readable"),
                                   tags$b("Select the following to compare runs")
                                ),
                                br(),
-                               selectInput(inputId = "compare_select_run2",
-                                           label = h4(paste0("Compare ",`runname1`," with")),
-                                           choices = runnames2
-                               ),
+                               uiOutput("compare_select_run2_ui"), # dynamic, lists runs based on input from selection page
                                h4("and select the following"),
                                selectInput(inputId = "compare_select_geography",
                                            label = "Geography",
@@ -34,7 +63,7 @@ navbarPage(theme = shinytheme("readable"),
                                            choices = years,
                                            selected = tail(years, n=1)), #select the last element of years
                                br(),
-                               helpText("Use the 'Box Select' or 'Lasso Select' option in the scatterplot to select 
+                               helpText("Use the 'Box Select' or 'Lasso Select' option in the scatterplot to select
                                         points and view its location on the map.")
                         ), # end column
                         column(width = 5,
@@ -44,7 +73,7 @@ navbarPage(theme = shinytheme("readable"),
                                leafletOutput("compare_map", height = "925px")
                         ) # end column
                       ) # end fluidRow
-                      
+
                     ) # end fluidPage
 
            ), # end tabPanel
@@ -56,10 +85,7 @@ navbarPage(theme = shinytheme("readable"),
                                   tags$b("Select the following to see growth since 2014")
                                ),
                                br(),
-                               selectInput(inputId = "growth_select_run",
-                                           label = "Run", 
-                                           choices = runs
-                               ),
+                               uiOutput("growth_select_run_ui"), # dynamic, lists runs based on input from selection page
                                selectInput(inputId = "growth_select_geography",
                                            label = "Geography",
                                            choices = c("TAZ"=1,
@@ -81,7 +107,7 @@ navbarPage(theme = shinytheme("readable"),
                                            step = 5,
                                            sep = ""),
                                br(),
-                               helpText("Use the 'Box Select' or 'Lasso Select' option in the scatterplot to select 
+                               helpText("Use the 'Box Select' or 'Lasso Select' option in the scatterplot to select
                                         points and view its location on the map.")
                         ), # end column
                         column(width = 5,
@@ -101,42 +127,42 @@ navbarPage(theme = shinytheme("readable"),
                               includeHTML('www/qc_ts_emp_sp.html')
                       ) # end tabPanel
            ), # end navbarMenu
-           tabPanel("Time Series",
-                      fluidPage(
-                        fluidRow(
-                          column(width = 2,
-                                 h4(class="header", checked=NA,
-                                    tags$b("Select the following to see time series trends by jurisdiction and indicator")
-                                 ),
-                                 br(),
-                                 selectInput(inputId = "select_tsplots",
-                                             label = "FAZ Large Area Groups",
-                                             choices = c("Eastside King (1)"=1,
-                                                         "Eastside King (2)"=2,
-                                                         "Green River"=3,
-                                                         "Seattle and Shoreline"=4,
-                                                         "SE King and King Other"=5,
-                                                         "SW King"=6,
-                                                         "Central, North, and South Kitsap"=7,
-                                                         "Peninsula and Tacoma"=8,
-                                                         "Pierce Other (1)"=9,
-                                                         "Pierce Other (2)"=10,
-                                                         "SW Pierce"=11,
-                                                         "Everett"=12,
-                                                         "NW Snohomish"=13,
-                                                         "Snohomish Other"=14,
-                                                         "SW Snohomish (1)"=15,
-                                                         "SW Snohomish (2)"=16
-                                             ),
-                                             selected = 1)
-
-                                 ), # end column
-                          column(width = 10,
-                                 htmlOutput("tsplots")
-                          ) # end column
-                        ) # end fluidRow
-              ) # end fluidPage
-           ), # end tabPanel
+           # tabPanel("Time Series",
+           #            fluidPage(
+           #              fluidRow(
+           #                column(width = 2,
+           #                       h4(class="header", checked=NA,
+           #                          tags$b("Select the following to see time series trends by jurisdiction and indicator")
+           #                       ),
+           #                       br(),
+           #                       selectInput(inputId = "select_tsplots",
+           #                                   label = "FAZ Large Area Groups",
+           #                                   choices = c("Eastside King (1)"=1,
+           #                                               "Eastside King (2)"=2,
+           #                                               "Green River"=3,
+           #                                               "Seattle and Shoreline"=4,
+           #                                               "SE King and King Other"=5,
+           #                                               "SW King"=6,
+           #                                               "Central, North, and South Kitsap"=7,
+           #                                               "Peninsula and Tacoma"=8,
+           #                                               "Pierce Other (1)"=9,
+           #                                               "Pierce Other (2)"=10,
+           #                                               "SW Pierce"=11,
+           #                                               "Everett"=12,
+           #                                               "NW Snohomish"=13,
+           #                                               "Snohomish Other"=14,
+           #                                               "SW Snohomish (1)"=15,
+           #                                               "SW Snohomish (2)"=16
+           #                                   ),
+           #                                   selected = 1)
+           # 
+           #                       ), # end column
+           #                column(width = 10,
+           #                       htmlOutput("tsplots")
+           #                ) # end column
+           #              ) # end fluidRow
+           #    ) # end fluidPage
+           # ), # end tabPanel
            tabPanel("Demographics",
                     fluidPage(
                       fluidRow(
@@ -171,9 +197,10 @@ navbarPage(theme = shinytheme("readable"),
                       fluidRow(h4(class="header", checked=NA,
                                   tags$b("Select the following to view the remaining developable capacity")
                       )),
+                      ###check dynamic UI here:
                       fluidRow(
                         column(width = 1,
-                               uiOutput("dcap_select_run") # dynamic, only runs with demographic indicators will be listed
+                               uiOutput("dcap_select_run") # dynamic, only runs with dev cap indicators will be listed
                         ), # end column
                         column(width = 1,
                                selectInput(inputId = "dcap_select_year",
