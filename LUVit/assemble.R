ea573b3a822effe9be9cb7549c8f14600995a3c1
Hana Sevcikova <hanas@uw.edu>
2023-06-14 10:11:26 -0700

    paths changed

--- LUVit/assemble.R
+++ LUVit/assemble.R
@@ -6,16 +6,16 @@ library(data.table)
 library(openxlsx2)
 
 # set working directory to the location of this source file (e.g. in Rstudio or using setwd())
-#setwd("I:/LandUseForecast/luvrepo/LUVit")
-setwd("~/psrc/ForecastProducts/luv/LUVit")
+setwd("I:/LandUseForecast/luvrepo/LUVit")
+#setwd("~/psrc/ForecastProducts/luv/LUVit")
 #setwd("C:/code_repos/luv/LUVit")
 
 # which run to use
 run <- "run_120.run_2023_05_11_12_57" 
 
 # where are the indicators
-#run.dir <- "N:/vision2050/opusgit/urbansim_data/data/psrc_parcel/runs/awsmodel04"
-run.dir <- "~/n$/vision2050/opusgit/urbansim_data/data/psrc_parcel/runs/awsmodel04"
+run.dir <- "N:/vision2050/opusgit/urbansim_data/data/psrc_parcel/runs/awsmodel04"
+#run.dir <- "~/n$/vision2050/opusgit/urbansim_data/data/psrc_parcel/runs/awsmodel04"
 
 # whole path to the group-quarters Excel file
 gq.file <- "Upd_OFM_SAEP_GQPop_Block_TAZ_Corres.xlsx" 
